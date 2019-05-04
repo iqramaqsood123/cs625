0dd7643f0ad60cdc3a62c37528f3ac635bb4244b
iqramaqsood123 <48157185+iqramaqsood123@users.noreply.github.com>
2019-05-04 11:51:08 -0700

    upload program

---
+++ payroll.java
@@ -0,0 +1,12 @@
+ public class payroll
+ {
+   public static void main(String[] args)    
+    {    
+ 	 int hours = 40;    
+	 double grossPay, payRate = 25.0;  
+
+	grossPay = hours * payRate;      
+	 System.out.println("Your gross pay is $" + grossPay); 
+	
+       } 
+  }
